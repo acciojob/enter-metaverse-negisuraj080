78c03461d9656bea85ed56119ebc24b67c73f59e
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-20 12:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+document.getElementById('enterBtn').addEventListener('click', function() {
+  // Change the content of the "p" tag with id "status"
+  document.getElementById('status').textContent = 'Entered Metaverse';
+});
+
