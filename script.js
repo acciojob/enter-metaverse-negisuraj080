53bf53e735238abe0afe9bf7626cd4554e376a0b
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-20 13:35:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-document.getElementById('enterBtn').addEventListener('click', function() {
-  document.getElementById('status').innerHTML = "<h1>Entered Metaverse</h1>";
-});
+// document.getElementById('enterBtn').addEventListener('click', function() {
+//   document.getElementById('status').innerHTML = "<h1>Entered Metaverse</h1>";
+// });
 
+
+
+function changingText() {
+	let pElement=document.getElementById("status");
+	pElement.innerHTML="<h1>Entered Metaverse</h1>";
+
+	
+}
